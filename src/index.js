beba52c24cc85135f1c211a794c4b0be604fe104
Yannick <ymulyangote@gmail.com>
2024-11-05 13:55:32 -0600

    Update menuLinks array

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@
 
 // Menu data structure
 var menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 
   //Part 1 - Getting Started - Build a main content element 
 const mainEl = document.querySelector("main");
@@ -34,6 +45,9 @@ const subMenuEl = document.querySelector("#sub-menu");
 subMenuEl.style.height = "100%";
 subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 subMenuEl.classList.add("flex-around");
+document.getElementById("sub-menu").style.position = "absolute";
+document.getElementById("sub-menu").style.top = "0";
+
 
 
 // Part 3 - Add Menu bars using menu date structure
