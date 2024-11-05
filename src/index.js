34476c4c70d410661d2a14dd6aac5d83f8f22b6f
Yannick <ymulyangote@gmail.com>
2024-11-05 01:33:26 -0600

    Delete file

---
+++ src/index.js
@@ -0,0 +1,40 @@
+//import "./styles.css";
+
+// Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+
+  //Part 1 - Getting Started - Build a main content element 
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+
+/* const h1El = document.createElement("h1");
+const h1Text = document.createTextNode ("DOM Manipulation");
+
+h1El.appendChild(h1Text);
+mainEl.appendChild(h1El); */
+
+mainEl.innerHTML = "<h1>DOM Manipulation<h1>";
+mainEl.classList.add("flex-ctr");
+
+// Part 2 - Create a Blank Menu Bar
+
+//let topMenuEl = document.createElement("nav");
+//topMenuEl.id = "top-menu";
+let topMenuEl = document.querySelector("#top-menu")
+topMenuEl.style.height = "100%"
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+
+// Part 3 - Add Menu bars using menu date structure
+menuLinks.forEach(link =>{
+    const a = document.createElement("a");
+    a.href = link.href;
+    a.textContent = link.text;
+    topMenuEl.append(a);
+});
